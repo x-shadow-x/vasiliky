9edc7915f4f858bde6c81e7172e920cc7b3dd4a1
x-shadow-x <2509344578@qq.com>
2016-05-20 18:42:42 +0800

    完成23个交互效果~并添加最后一个item翻页效果

--- src/js/lastest.js
+++ src/js/lastest.js
@@ -1,3 +1,107 @@
 module.controller('lastestCtrl', ['$scope', function($scope) {
-	
+
+	$scope.list = [
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+		{
+			title: 'Nice',
+			text: 'Lily likes to play with crayons and pencils'
+		},
+	];
+	function loadApp() {
+		$('.flipbook').turn({});
+	}
+	$(function() {
+		yepnope({
+			test : Modernizr.csstransforms,
+			yep: ['src/js/lib/turn.min.js'],
+			complete: loadApp
+		});
+	})
 }]);
